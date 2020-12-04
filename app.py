1cdca03b1f1080654a4f5e963abe781c76dad6b8
serena-123 <70485241+serena-123@users.noreply.github.com>
2020-12-03 21:28:31 -0500

    Working App
    Unit is finally working

--- app.py
+++ app.py
@@ -1,48 +1,59 @@
-import os
+import os, datetime 
+from datetime import datetime
+datetime.utcnow()
 from flask import Flask, render_template, session, redirect, request, url_for
 from flask_bootstrap import Bootstrap
-# from flask_moment import Moment
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func, desc
+from sqlalchemy.sql.expression import func
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Exercises.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Unit.db'
 app.config['SQLALCHEMY_BINDS'] = {
-	'block':        'sqlite:///Block.db',
-	'workout':      'sqlite:///Workout.db'
+	'type':        'sqlite:///Type.db',
+	'workout':      'sqlite:///Workout.db',
 }
 
 app.config['SECRET_KEY'] = 'secret string'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Exercises.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
-# migrate = Migrate(app, db)
 
 
-class Exercises(db.Model):
+# class Users(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	name = db.Column(db.String(50), nullable=False)
+
+
+class Type(db.Model):
+	__bind_key__='type'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(50), nullable=False)
 
 	def __repr__(self):
 		return '<Name %r>' % self.name
 
-class Block(db.Model):
-	__bind_key__ = 'block'
-	id = db.Column(db.Integer, primary_key=True)
+
+class Unit(db.Model):
+	# __bind_key__='unit'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'))
+	type_id = db.Column(db.Integer, db.ForeignKey('type.id')) 
 	weight = db.Column(db.Integer)
-	reps = db.Column(db.Integer)
-	exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id')) 
+	reps = db.Column(db.Integer) 
 
 
-class Workout(db.Model): # should add user and date maybe?
+class Workout(db.Model):
 	__bind_key__ = 'workout'
 	id = db.Column(db.Integer, primary_key=True)
-	exercise_block = db.Column(db.Integer, db.ForeignKey('block.id'))
-	# exercise_set = db.relationship('Block', backref='workout', lazy='dynamic')
+	date = db.Column(db.DateTime, nullable=True)
+	# user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #natalie's User table 
+	# exerciseunit_relationship = db.relationship('Unit', backref='workout', lazy='dynamic')
+
+
+
 
 
 @app.route('/')
@@ -50,13 +61,19 @@ def home():
 	title = "Home"
 	return render_template("home.html")
 
+@app.route('/history')
+def history():
+	title = "History"
+	units = Unit.query.all()
+	return render_template("history.html", units=units)
+
 
 @app.route('/exercises', methods=['POST', 'GET'])
 def exercises():
 	title = "Exercises"
 	if request.method == "POST": 
 		ex_name = request.form['name']
-		new_ex = Exercises(name=ex_name)
+		new_ex = Type(name=ex_name)
 		try:
 			db.session.add(new_ex)
 			db.session.commit()
@@ -64,52 +81,63 @@ def exercises():
 		except:
 			return "there was an error"
 	else:
-		exercises = Exercises.query.all()
+		exercises = Type.query.all()
 		return render_template("exercises.html", title=title, exercises=exercises)
 
 
 
+@app.route('/createworkout', methods=['POST', 'GET'])
+def createworkout():
+	if request.method == "POST":
+		current_workout = Workout(date=datetime.utcnow())
+
+		try: 
+			db.session.add(current_workout)
+			db.session.commit()
+			return redirect(url_for("addworkout"))
+		except: 
+			return "didnt work"
+	else:
+		# workouts.Workout.query.all()
+		return render_template("home.html")
+
+
 
 @app.route('/addworkout', methods=['POST', 'GET'])
-def addworkout(): # trying to shuffle info into Block database 
+def addworkout():  
 	title = "Add Workout"
+
+	workout_id = Workout.query.order_by(Workout.id.desc()).first() 
+	get_workout_id = str(workout_id.id)
+
 	if request.method == "POST":
 
 		get_weight = request.form['weight1']
 		get_reps = request.form['reps1']
-		get_exercise = request.form['exercise1']
+		get_type = request.form['exercise1']
 
-		block_unit = Block(weight=get_weight, reps=get_reps, exercise_id=get_exercise)
+		exercise_unit = Unit(weight=get_weight, reps=get_reps, type_id=get_type, workout_id=get_workout_id)
 
 		try:
-			db.session.add(block_unit)
-			db.session.commit() 
+			db.session.add(exercise_unit)
+			db.session.commit()
 			return redirect(url_for('addworkout'))
 		except:
-			return "there was an error"
+			return "fail"
+
 	else:
-		blocks = Block.query.all()
-		exercises = Exercises.query.all()
-		return render_template("addworkout.html", exercises=exercises, blocks=blocks)
+		exercises = Type.query.all()
+		units = Unit.query.all()
+		workouts = Workout.query.all()
+		return render_template("addworkout.html", exercises=exercises, units=units, workouts=workouts)
 
 
 
 
-
-
-@app.route('/delete/<int:id>')
-def delete_block(id):
-    block_delete = Block.query.get_or_404(id)
-    try:
-        db.session.delete(block_delete)
-        db.session.commit()
-        return redirect(url_for('addworkout'))
-    except:
-        return "there was a problem deleting"
 
 @app.route('/delete_ex/<int:id>')
 def delete_ex(id):
-    exercise_delete = Exercises.query.get_or_404(id)
+    exercise_delete = Type.query.get_or_404(id)
     try:
         db.session.delete(exercise_delete)
         db.session.commit()
