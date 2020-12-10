f9a950cc6a11c0b2cbf9f24f900805d04eda6397
nolechno <nolechno@u.rochester.edu>
2020-12-09 21:24:32 -0600

    finished draft

--- flasky.py
+++ flasky.py
@@ -4,7 +4,6 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Type, Unit, Workout
 
-app=application
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
